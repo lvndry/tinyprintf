b55b6ad29733794ceed879d95be87886f52d3a02
Landry Monga <landry.monga@epita.fr>
2019-09-16 18:49:12 +0200

    write signed and unsigned

--- tinyprintf.c
+++ tinyprintf.c
@@ -5,6 +5,18 @@
 
 #define BUFFER_SIZE 128
 
+int intlen(int n)
+{
+    int len = 0;
+     if(n < 0) n = -n;
+     while(n > 0)
+     {
+         len++;
+         n /= 10;
+     }
+    return len;
+ }
+
 void buffer_flush(char buffer[], unsigned int *size)
 {
     if (buffer == NULL)
@@ -29,10 +41,69 @@ int buffer_write(char buffer[], char c, unsigned int *size)
     return 1;
 }
 
+int buffer_write_int(char buffer[], int n, unsigned *size)
+{
+    int len = intlen(n);
+
+    if ((*size + len) > BUFFER_SIZE)
+    {
+        buffer_flush(buffer, size);
+    }
+
+    if (n < 0)
+    {
+        buffer_write(buffer, '-', size);
+        n = -n;
+    }
+
+    for (int i = len-1; i >= 0; i--)
+    {
+        buffer[(*size)+i] = (n%10) + 48;
+        n /= 10;
+    }
+
+    *size += len;
+
+    return 1;
+}
+
+int tiny_strlen(const char *str)
+{
+    int len = 0;
+
+    while(str[len] != '\0')
+    {
+        len++;
+    }
+
+    return len;
+}
+
+char* tiny_itoa(int n, char *str)
+{
+    int len = intlen(n);
+    int num;
+    int index = len;
+
+    if (n == 0) return "0";
+
+    while(n > 0)
+    {
+       num = n%10;
+       str[index-1] = num;
+       n /= 10;
+       index--;
+    }
+
+    return str;
+}
+
 int tinyprintf(const char *format, ...) {
     va_list ap;
     char *string = NULL;
     char letter;
+    int sint;
+    unsigned uint;
     char buffer[BUFFER_SIZE];
     unsigned int buffer_size = 0;
     int output_len = 0;
@@ -52,11 +123,22 @@ int tinyprintf(const char *format, ...) {
             {
                 case 's':
                     string = va_arg(ap, char *);
-                    fputs(string, stdout);
+                    for (int i = 0; i < tiny_strlen(string); i++)
+                    {
+                        buffer_write(buffer, string[i], &buffer_size);
+                    }
                     break;
                 case 'c':
                     letter = va_arg(ap, int);
-                    putchar(letter);
+                    buffer_write(buffer, letter, &buffer_size);
+                break;
+                case 'd':
+                    sint = va_arg(ap, int);
+                    buffer_write_int(buffer, sint, &buffer_size);
+                break;
+                case 'u':
+                    uint = va_arg(ap, unsigned);
+                    buffer_write_int(buffer, uint, &buffer_size);
                 break;
                 default:
                     buffer_write(buffer, '%', &buffer_size);
@@ -81,7 +163,7 @@ int tinyprintf(const char *format, ...) {
 
 int main(void)
 {
-    tinyprintf("Hello %s it'working fine!", "landry");
+    tinyprintf("Hello %s it'working fine! %d is a signed int and %u unsigned", "landry", -15, 2);
 
     return 0;
 }
